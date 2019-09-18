72f96ee4b431bfc68be0019fa7e1eda7d60b0701
Mike Kazantsev <mk.fraggod@gmail.com>
2019-09-18 06:29:11 +0500

    mpv.lavfi-audio-vis: integrate pre-exising --lavfi-complex value if it's simple enough, add hotkey to toggle that

--- mpv/fg.lavfi-audio-vis.lua
+++ mpv/fg.lavfi-audio-vis.lua
@@ -1,4 +1,5 @@
 -- Auto-enable fancy visualization for audio files in mpv if window/vo is forced/enabled
+-- https://github.com/mpv-player/mpv/blob/master/player/lua/defaults.lua
 
 local function str_split(str, pat)
 	local res = {}
@@ -31,6 +32,7 @@ local function lavfi_filter_string(spec)
 end
 
 local function lavfi_vis_for_audio()
+	-- Disable filter if there's no vo or it's being used for any other output
 	if not mp.get_property_bool('vo-configured') then return end
 
 	local aid_n, vid_n, art_n = 0, 0, 0
@@ -47,6 +49,30 @@ local function lavfi_vis_for_audio()
 	end
 
 	if vid_n > 0 then return end
+
+	-- vis - visualization part, src - pre-exising part, src_disabled - flag to toggle the former
+	local lavfi_vis, lavfi_src, lavfi_src_disabled
+	local function lavfi_update(src_toggle)
+		if src_toggle then lavfi_src_disabled = not lavfi_src_disabled end
+		src_toggle = '[aid1]'..(lavfi_src_disabled and '' or lavfi_src or '')..lavfi_vis
+		-- mp.msg.info('----- filter: '..src_toggle)
+		mp.set_property('options/lavfi-complex', src_toggle)
+	end
+
+	-- Check for existing aid1->ao --lavfi-complex filter and include that in resulting pipeline
+	-- Aborts if pipeline looks too complex to include in the resulting filter
+	-- Note the dynamic keybinding for toggling pre-vis filter (if any), it should override input.conf
+	lavfi_src = mp.get_property('options/lavfi-complex') or ''
+	if lavfi_src and #lavfi_src > 0 then
+		lavfi_src = lavfi_src:match('^ *%[ *aid1 *%] *(.-) *%[ *ao *%] *$')
+	end
+	if not lavfi_src then
+		mp.msg.info('Non-trivial --lavfi-complex ("[aid1] ... [ao]") used, audio visualization disabled')
+		return
+	elseif #lavfi_src > 0 then
+		lavfi_src = ' '..lavfi_src..','
+		mp.add_key_binding('y', function() lavfi_update(true) end)
+	end
 
 	-- Filter Docs: https://ffmpeg.org/ffmpeg-filters.html
 	-- Fancy Examples: https://trac.ffmpeg.org/wiki/FancyFilteringExamples
@@ -66,7 +92,7 @@ local function lavfi_vis_for_audio()
 			sono_g = 4,
 			bar_v = 9,
 			sono_v = 17,
-			font = "'Luxi Sans,Liberation Sans,Sans|bold'",
+			font = "'Liberation Mono,Luxi Mono,Monospace|bold'", -- has to be monospace
 			fontcolor = "'st(0, (midi(f)-53.5)/12); st(1, 0.5 - 0.5 * cos(PI*ld(0))); r(1-ld(1)) + b(ld(1))'",
 			tc = '0.33',
 			tlength = "'st(0,0.17); 384*tc / (384 / ld(0) + tc*f /(1-ld(0))) + 384*tc / (tc*f / ld(0) + 384 /(1-ld(0)))'" } }
@@ -75,16 +101,16 @@ local function lavfi_vis_for_audio()
 		{mode='lissajous_xy', size='960x200', rate=30, scale='cbrt', draw='dot', zoom=1.5} }
 
 	local overlay = lavfi_filter_string{'overlay', {format='yuv420'}}
-	local lavfi = '[aid1] asplit=3 [ao][a1][a2]; [a1]'..filter_bg..'[v1]; [a2]'..filter_fg..'[v2]; [v1][v2]'..overlay..'[vo]'
+	lavfi_vis = ' asplit=3 [ao][a1][a2]; [a1] '..
+		filter_bg..' [v1]; [a2] '..filter_fg..' [v2]; [v1][v2] '..overlay..' [vo]'
 
 	-- local filter = lavfi_filter_string{ 'showspectrum',
 	-- 	{size='960x758', color='channel', scale='cbrt', orientation='vertical', overlap=1} }
 	-- local filter = lavfi_filter_string{'showwaves', {size='960x384', rate=60, mode='point', n=10, scale='lin'}}
-	-- mp.msg.info('----- filter: '..filter)
 	-- local lavfi = '[aid1] asplit [ao][vis]; [vis]'..filter..'[vo]'
 	-- local lavfi = '[aid1] asplit=3 [ao][a1][a2]; [a1]'..filter1..'[v1]; [a2]'..filter2..'[v2]; [v1][v2] vstack [vo]'
 
-	mp.set_property('options/lavfi-complex', lavfi)
+	lavfi_update()
 end
 
 if str_split(mp.get_property('vo'), '[^,]+')[1] ~= 'null'
