a714ab8673d9dd393b9a1188fcdf57932eb430a4
Mike Kazantsev <mk.fraggod@gmail.com>
2020-02-22 17:28:12 +0500

    xbindkeys: add debouncing for remapped pagedown mouse button

--- xbindkeys.scm
+++ xbindkeys.scm
@@ -1,9 +1,8 @@
+;;;; -- Debouncing script for crappy Razer mouse
+
 (define (mono-time)
 	"Return monotonic timestamp in seconds as real."
 	(+ 0.0 (/ (get-internal-real-time) internal-time-units-per-second)))
-
-
-;;;; -- Mouse-2 (middle button) click debouncing for crappy Razer mouse
 
 (define razer-delay-min 0.2)
 (define razer-wait-max 0.5)
@@ -11,14 +10,14 @@
 (define razer-ts-done #f)
 (define razer-debug #f)
 
-(xbindkey-function "b:8" (lambda ()
+(define razer-press (lambda ()
 	(let ((ts (mono-time)))
 		(when
 			;; Enforce min ts diff between "done" and "start" of the next one
 			(or (not razer-ts-done) (>= (- ts razer-ts-done) razer-delay-min))
 			(set! razer-ts-start ts)))))
 
-(xbindkey-function '(Release "b:8") (lambda ()
+(define razer-release (lambda (command)
 	(let ((ts (mono-time)))
 		(when razer-debug
 			(format #t "razer: ~a/~a delay=~a[~a] wait=~a[~a]\n"
@@ -33,7 +32,14 @@
 				(and razer-ts-start (<= (- ts razer-ts-start) razer-wait-max)))
 			(set! razer-ts-done ts)
 			(when razer-debug (format #t "razer: --- click!\n"))
-			(run-command "xdotool click 2"))))) ;; XXX: something more liteweight maybe?
+			;; XXX: something more liteweight than shell + xdotool maybe?
+			(run-command command)))))
+
+
+;;;; -- Mouse-2 (middle button) - bound as mouse-8
+
+(xbindkey-function "b:8" (lambda () (razer-press)))
+(xbindkey-function '(Release "b:8") (lambda () (razer-release "xdotool click 2")))
 
 ;; (xbindkey '("b:2") "echo click2+ >> ~/evtest.log")
 ;; (xbindkey '(Release "b:2") "echo click2- >> ~/evtest.log")
@@ -41,4 +47,5 @@
 
 ;;;; -- Mouse-9 to act as a "Page Down" button event
 
-(xbindkey "b:9" "xdotool key Next")
+(xbindkey-function "b:9" (lambda () (razer-press)))
+(xbindkey-function '(Release "b:9") (lambda () (razer-release "xdotool key Next")))
